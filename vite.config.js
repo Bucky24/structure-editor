0192b095301061bf5203e71a6389089ad385b573
Robbert Wijtman <rwijtman@slack-corp.com>
2025-03-24 13:56:32 -0700

    build with types

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   server: {
     port: 8080,
   },
